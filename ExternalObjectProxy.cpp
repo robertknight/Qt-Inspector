193a48879b092d227b9cabd4de7cdbb436596e0f
Robert Knight <robertknight@gmail.com>
2011-07-16 17:35:37 +0100

    Flesh out the ExternalObjectProxy implementation.
     * Add accessors which TargetApplicationProxy can use to set the proxy's properties
       when it has fetched them from the target process.

--- ExternalObjectProxy.cpp
+++ ExternalObjectProxy.cpp
@@ -1,2 +1,84 @@
 #include "ExternalObjectProxy.h"
 
+#include "TargetApplicationProxy.h"
+
+ExternalObjectProxy::ExternalObjectProxy(TargetApplicationProxy* appProxy, int objectId)
+: m_appProxy(appProxy)
+, m_objectId(objectId)
+, m_isLoaded(false)
+{
+}
+
+void ExternalObjectProxy::setLoaded(bool loaded)
+{
+	m_isLoaded = loaded;
+}
+
+int ExternalObjectProxy::objectId() const
+{
+	return m_objectId;
+}
+
+void ExternalObjectProxy::setClassName(const QString& className)
+{
+	m_className = className;
+}
+
+void ExternalObjectProxy::setObjectName(const QString& objectName)
+{
+	m_objectName = objectName;
+}
+
+void ExternalObjectProxy::setProperty(const QString& name, const QVariant& value)
+{
+	m_properties.insert(name,value);
+}
+
+void ExternalObjectProxy::setChildIds(const QList<int>& childIds)
+{
+	m_childIds = childIds;
+}
+
+QString ExternalObjectProxy::className() const
+{
+	doLoad();
+	return m_className;
+}
+
+QString ExternalObjectProxy::objectName() const
+{
+	doLoad();
+	return m_objectName;
+}
+
+QVariant ExternalObjectProxy::readProperty(const QString& name) const
+{
+	doLoad();
+	return m_properties.value(name);
+}
+
+void ExternalObjectProxy::writeProperty(const QString& name, const QVariant& value)
+{
+	doLoad();
+	m_properties.insert(name,value);
+}
+
+QList<ObjectProxy*> ExternalObjectProxy::children()
+{
+	QList<ObjectProxy*> result;
+	Q_FOREACH(int childId, m_childIds)
+	{
+		result << m_appProxy->fetchProxy(childId);
+	}
+	return result;
+}
+
+bool ExternalObjectProxy::doLoad() const
+{
+	if (m_isLoaded)
+	{
+		return true;
+	}
+	return m_appProxy->fetchObject(const_cast<ExternalObjectProxy*>(this));
+}
+
--- ExternalObjectProxy.h
+++ ExternalObjectProxy.h
@@ -9,10 +9,30 @@ class ExternalObjectProxy : public ObjectProxy
 	public:
 		ExternalObjectProxy(TargetApplicationProxy* appProxy, int objectId);
 
+		int objectId() const;
+		void setClassName(const QString& className);
+		void setObjectName(const QString& objectName);
+		void setProperty(const QString& name, const QVariant& value);
+		void setChildIds(const QList<int>& children);
+		void setLoaded(bool loaded);
+
 		virtual QString className() const;
 		virtual QString objectName() const;
 		virtual QVariant readProperty(const QString& name) const;
 		virtual void writeProperty(const QString& name, const QVariant& value);
 		virtual QList<ObjectProxy*> children();
+
+	private:
+		bool doLoad() const;
+
+		TargetApplicationProxy* m_appProxy;
+		int m_objectId;
+		bool m_isLoaded;
+
+		QString m_className;
+		QString m_objectName;
+		QHash<QString,QVariant> m_properties;
+		QList<int> m_childIds;
+
 };
 
